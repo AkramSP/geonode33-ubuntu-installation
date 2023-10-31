42e9c26c535e74b96dbaae2f59ba81fd26b8367f
AkramSP <74866918+AkramSP@users.noreply.github.com>
2023-10-31 11:18:28 +0700

    Update install-geonode33-part1.sh

--- install-geonode33-part1.sh
+++ install-geonode33-part1.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+sudo apt install -y python3-gdal=3.3.2+dfsg-2~focal2 gdal-bin=3.3.2+dfsg-2~focal2 libgdal-dev=3.3.2+dfsg-2~focal2
+sudo apt install -y libxml2 libxml2-dev gettext
+sudo apt install -y libxslt1-dev libjpeg-dev libpng-dev libpq-dev
+sudo apt install -y software-properties-common build-essential
+sudo apt install -y git unzip gcc zlib1g-dev libgeos-dev libproj-dev
+sudo apt install -y sqlite3 spatialite-bin libsqlite3-mod-spatialite
 
-sudo apt-get update -y
-sudo apt-get install -y software-properties-common
 sudo add-apt-repository universe
+sudo apt-get update -y
 sudo apt-get install -y git-core git-buildpackage debhelper devscripts
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -17,4 +21,4 @@ sudo apt autoremove --purge
 sudo service docker start
 
 sudo usermod -aG docker $USER
-sudo su - $USER
+su - $USER
