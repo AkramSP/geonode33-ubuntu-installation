98bb4b35b119ae16b480d0df0595b49524fdd10b
AkramSP <74866918+AkramSP@users.noreply.github.com>
2023-10-31 11:26:10 +0700

    Update install-geonode33-part2.sh

--- install-geonode33-part2.sh
+++ install-geonode33-part2.sh
@@ -8,6 +8,8 @@ sudo chmod -Rf 775 /opt/geonode/
 cd /opt
 git clone https://github.com/GeoNode/geonode.git -b 3.3.x geonode
 
+pip install Django==2.2.24
+
 cd /opt/geonode
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml up -d
